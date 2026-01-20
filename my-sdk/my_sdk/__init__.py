773a344cad2df02ae411e7e003396aea89ecc6d8
Silvio <silvio.estat@gmail.com>
2026-01-20 11:55:26 -0300

    Criando um decorator

---
+++ my-sdk/my_sdk/__init__.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+__version__ = "0.0.1"
+__all__ = ["sql"]
+
+def get_provider_info() -> dict[str, Any]:
+    return{
+        "package-name":"my_sdk",
+        "name": "My SDK",
+        "description":"My SDK is a package that provides a set of tools for building Airflow DAGs.",
+        "vbersion": [__version__],
+        "task-decorators": [
+                {
+                    "name": "sql",
+                    "class-name": "my_sdk.decorators.sql.sql_task"
+                }
+
+        ]
+    }
